9ec52e74b6ded8f688651f2089f2a36c5a32be30
andoniVT <andoni.valverde@ucsp.edu.pe>
2014-10-29 14:33:03 -0500

    unsupervised finished

--- unsupervised/manager.py
+++ unsupervised/manager.py
@@ -1,5 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 '''
 Created on 26/10/2014
 
-@author: andoni
+@author: Andoni Valverde Tohalino
+@email: andoni.valverde@ucsp.edu.pe
+
 '''
+
+from unsupervised.classifier import Classifier
+
+class UnsupervisedManager:
+    def __init__(self):
+        pass
+    
+    def classify_comment(self , comment):
+        obj = Classifier()
+        obj.classify(comment)
+        sentiment = obj.get_label()
+        return sentiment 
+
+if __name__ == '__main__':
+    
+    classifier = UnsupervisedManager()
+    comentario = "La tercera fue la vencida!! Felicidades al Real Madrid por ganar la Copa al eterno rival"
+    comentario2 = "es un desastre pesimo"
+    print classifier.classify_comment(comentario2)
+    
+ 
